a7466a7bdf6363d59c9df77e6275881a6719f310
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2020-02-26 20:02:39 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const EXCHANGE_RATES = gql`
+	{
+		rates(currency: "USD") {
+			currency
+			rate
+		}
+	}
+`;
+
+const App = () => {
+	const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+	console.log(data);
+	return <div></div>;
+};
 
 export default App;
