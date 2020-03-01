8ff77f4c99d7713af3b92bc5367fe93384ab1d10
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2020-03-02 01:27:26 +0600

    some layouts

--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -1,45 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { AppBar, Toolbar, Button, Grid } from '@material-ui/core';
 import { useLocation, useHistory } from 'react-router-dom';
 
 const TopBar = () => {
-  const { pathname } = useLocation();
-  const { push } = useHistory();
-  return (
-    <>
-      {pathname === '/' && (
-        <AppBar position='static'>
-          <Grid container justify='flex-end'>
-            <Toolbar>
-              <Button color='inherit' onClick={() => push('/signin')}>
-                Войти
-              </Button>
-              <Button
-                color='default'
-                variant='contained'
-                onClick={() => push('/signup')}
-              >
-                Зарегистрироваться
-              </Button>
-            </Toolbar>
-          </Grid>
-        </AppBar>
-      )}
-      {pathname === '/signin' && (
-        <AppBar color='transparent'>
-          <Grid container justify='space-between' alignItems='center'>
-            <div>Icon</div>
-            <Toolbar>
-              <Button color='default' onClick={() => push('/signup')}>
-                Зарегистрироваться
-              </Button>
-            </Toolbar>
-          </Grid>
-        </AppBar>
-      )}
-    </>
-  );
+	const { pathname } = useLocation();
+	const { push } = useHistory();
+	const authText = pathname === '/signup' ? 'Вход' : 'Зарегистрироваться';
+	const authLink = pathname === '/signup' ? '/signin' : '/signup';
+
+	useEffect(() => {
+		document.title = authText
+	}, []);
+
+	return (
+		<>
+			{pathname === '/' && (
+				<AppBar position="static">
+					<Grid container justify="flex-end">
+						<Toolbar>
+							<Button color="inherit" onClick={() => push('/signin')}>
+								Войти
+							</Button>
+							<Button color="default" variant="contained" onClick={() => push('/signup')}>
+								Зарегистрироваться
+							</Button>
+						</Toolbar>
+					</Grid>
+				</AppBar>
+			)}
+			{(pathname === '/signin' || pathname === '/signup') && (
+				<AppBar color="transparent" position="static">
+					<Grid container justify="space-between" alignItems="center">
+						<div>Icon</div>
+						<Toolbar>
+							<Button color="default" onClick={() => push(authLink)}>
+								{authText}
+							</Button>
+						</Toolbar>
+					</Grid>
+				</AppBar>
+			)}
+		</>
+	);
 };
 
 export default TopBar;
--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
+import { Topbar } from '../../components';
+import { Container, Grid, Typography, Button, TextField } from '@material-ui/core';
+
+import './index.css';
+
 const SignUp = () => {
-	return <div>SignUp</div>;
+	return (
+		<>
+			<Topbar />
+			<Container maxWidth="sm" className="auth__container">
+				<Typography component="h1" variant="h5">
+					Регистрация
+				</Typography>
+				<Grid container direction="row" justify="center" alignItems="center">
+					<form noValidate>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<Button type="submit" fullWidth variant="contained" color="primary">
+							Sign In
+						</Button>
+					</form>
+				</Grid>
+			</Container>
+		</>
+	);
 };
 
 export default SignUp;
